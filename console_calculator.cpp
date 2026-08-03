ec924ef7b8e0e1b1e99bc5e30e37c510368a9852
Adnan Anjum <anjumcode12@gmail.com>
2026-08-03 15:54:28 +0500

    add temperature convertor (F-C) program

--- console_calculator.cpp
+++ console_calculator.cpp
@@ -1,6 +1,10 @@
+#include <cmath>
+#include <iomanip>
 #include <iostream>
 using std::cin;
 using std::cout;
+using std::fixed;
+using std::setprecision;
 int main() {
 
   char op;
@@ -9,13 +13,20 @@ int main() {
   double result;
 
   cout << "***************** Calculator ***************\n";
+
+  cout << fixed << setprecision(10);
+  cout << "Choose operator (+ _ * /) :";
+  cin >> op;
+  while (
+
+      op != '+' && op != '-' && op != '*' && op != '/') {
+    cout << "Please choose an operator first\n";
+    cin >> op;
+  }
   cout << "Please enter the first number :";
   cin >> a;
   cout << "Please enter the second number :";
   cin >> b;
-
-  cout << "Choose operator (+ _ * /)" << '\n';
-  cin >> op;
   switch (op) {
   case '+':
     result = a + b;
@@ -31,7 +42,12 @@ int main() {
     break;
   case '/':
     result = a / b;
-    cout << "Quotient of both numbers is : " << result << '\n';
+    if (std::isinf(result)) {
+      cout << "Infinite Result\n";
+    } else {
+
+      cout << "Quotient of both numbers is : " << result << '\n';
+    }
     break;
   default:
     cout << "Invalid operator" << '\n';
---
+++ temperature_convertor.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+int main() {
+  char unit;
+  double temperature;
+  cout << "************Temperature Convertor ****************\n";
+  cout << "F = Fahrenheit" << '\n';
+  cout << "C = Celsius" << '\n';
+  cout << "Choose the unit (C/F) :";
+  cin >> unit;
+  if (unit == 'C' || unit == 'c') {
+    cout << "Enter the temperature in Fahrenheit :";
+    cin >> temperature;
+    temperature = (temperature - 32) / 1.8;
+    cout << "Temperature in Celsius is : " << temperature << "C" << '\n';
+  } else if (unit == 'F' || unit == 'f') {
+    cout << "Enter the temperature in Celsius :";
+    cin >> temperature;
+    temperature = (temperature * 1.8) + 32;
+    cout << "Temperature in Fahrenheit is : " << temperature << "F" << '\n';
+  } else {
+    cout << "Invalid unit choice\n";
+  }
+  return 0;
+}
