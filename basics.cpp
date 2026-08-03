69db5e1a5a0b748aea5746a7b60e8bb8293e6100
Adnan Anjum <anjumcode12@gmail.com>
2026-08-03 12:54:45 +0500

    add initial programs while learning cpp

---
+++ basics.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <iostream>
+// using std::cin;
+using std::cout;
+using std::string;
+int main() {
+  // double x = 3.7;
+  // double y = 11;
+
+  double z;
+
+  // z = std::max(x, y);
+  // z = std::min(x, y);
+  // z = pow(3, 3);
+  // z = sqrt(12);
+  // z = abs(-10);
+  // z = round(x); // rounds to the nearest whole integer;
+  // z = ceil(x);// ceil rounds up;
+  // z = floor(x);// floor roundes down
+  z = round(-4.1);
+  cout << "The value of z is : " << z << '\n';
+
+  return 0;
+}
