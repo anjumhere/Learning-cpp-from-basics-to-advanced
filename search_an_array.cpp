c55164ba4c6391d2a6ccfab6a6ff258ad519c406
Adnan Anjum <anjumcode12@gmail.com>
2026-08-11 21:25:43 +0500

    add array search program

---
+++ search_an_array.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+
+using std::cin;
+using std::cout;
+
+int startSearch(int nums[], int arraySize, int searchItem);
+int main() {
+
+  int nums[] = {44, 22, 5, 3, 6666, 7, 5, 343, 2};
+  int arraySize = sizeof(nums) / sizeof(nums[0]);
+  int searchItem;
+  cout << "Enter the number you want to serach : ";
+  cin >> searchItem;
+  int index = startSearch(nums, arraySize, searchItem);
+
+  if (index != -1) {
+    cout << "The number :" << searchItem << " is at index :" << index << '\n';
+
+  } else {
+    cout << "Number not found.\n";
+  }
+  return 0;
+}
+
+int startSearch(int nums[], int arraySize, int searchItem) {
+
+  for (int i = 0; i < arraySize; i++) {
+    if (nums[i] == searchItem) {
+      return i;
+    }
+  }
+  return -1;
+}
