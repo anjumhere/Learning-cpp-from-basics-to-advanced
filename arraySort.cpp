41dc1ad736e58a4245cf2ebd864307765de1848f
Adnan Anjum <anjumcode12@gmail.com>
2026-08-11 22:36:08 +0500

    tried sorting algorithm

--- arraySort.cpp
+++ arraySort.cpp
@@ -1,14 +1,15 @@
+// #include <algorithm>
 #include <iostream>
-
 using std::cout;
+// using std::sort;
 
 void sort(int arr[], int size);
 int main() {
 
   int arr[] = {6, 5, 4, 3, 7, 2, 8, 11, 14, 13, 12, 15, 16, 1, 9, 0};
   int size = sizeof(arr) / sizeof(arr[0]);
+  // sort(arr, arr + size);
   sort(arr, size);
-
   for (int elem : arr) {
     cout << elem << '\n';
   }
