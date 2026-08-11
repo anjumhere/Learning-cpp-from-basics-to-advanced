252adaab7f43e749fb31db01c79d3f62e4d4976d
Adnan Anjum <anjumcode12@gmail.com>
2026-08-11 22:33:10 +0500

    moved sorting algorithm to function

--- arraySort.cpp
+++ arraySort.cpp
@@ -2,11 +2,20 @@
 
 using std::cout;
 
+void sort(int arr[], int size);
 int main() {
 
   int arr[] = {6, 5, 4, 3, 7, 2, 8, 11, 14, 13, 12, 15, 16, 1, 9, 0};
   int size = sizeof(arr) / sizeof(arr[0]);
+  sort(arr, size);
 
+  for (int elem : arr) {
+    cout << elem << '\n';
+  }
+  return 0;
+}
+
+void sort(int arr[], int size) {
   for (int i = 0; i < size - 1; i++) {
     for (int j = 0; j < size - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
@@ -16,9 +25,4 @@ int main() {
       }
     }
   }
-
-  for (int i = 0; i < size; i++) {
-    cout << arr[i] << '\n';
-  }
-  return 0;
 }
