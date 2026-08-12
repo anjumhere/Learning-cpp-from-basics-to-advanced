ecaf61fe23e34e5362f6b97c50f74da1494abfe0
Adnan Anjum <anjumcode12@gmail.com>
2026-08-12 23:35:51 +0500

    add 2d array iteration

--- multi_dimentional_arrays.cpp
+++ multi_dimentional_arrays.cpp
@@ -5,8 +5,24 @@ using std::string;
 
 int main() {
   // c1    // c2    // c3
+  /*
   string cars[3][3] = {{"Supra", "Gt", "Mustang"},                // rows
                        {"f1", "bmw", "ferrari"},                  // rows
                        {"honda", "lamborgini", "astron martni"}}; // rows
+
+  */
+
+  int nums[3][4] = {{1, 2, 3, 4}, {4, 3, 2, 1}, {3, 4, 1, 2}};
+  int rows = sizeof(nums) / sizeof(nums[0]);
+  int columns = sizeof(nums[0]) / sizeof(nums[0][0]);
+
+  // iterating over 2d array
+
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < columns; j++) {
+      cout << nums[i][j] << " ";
+    }
+    cout << '\n';
+  }
   return 0;
 }
