83f0b291c84a9c0b94c9bb96cd398d589cecd63e
Adnan Anjum <anjumcode12@gmail.com>
2026-08-14 01:24:27 +0500

    add factorial using both recursive and iterative approach

---
+++ recursion.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using std::cout;
+
+void walk(int num);
+int iterative(int val);
+int recursive(int val);
+int main() {
+  // walk(100);
+  // finding a factorial using both recursive and iterative methods
+  cout << iterative(10) << '\n';
+  cout << recursive(10) << '\n';
+  return 0;
+}
+
+void walk(int num) {
+  if (num > 0) {
+    cout << "You walked a step\n";
+    walk(num - 1);
+  }
+}
+int iterative(int val) {
+  int result = 1;
+  for (int i = 1; i <= val; i++) {
+    result *= i;
+  }
+
+  return result;
+}
+int recursive(int val) {
+  if (val > 0) {
+    val *= recursive(val - 1);
+    return val;
+  } else {
+    return 1;
+  }
+}
