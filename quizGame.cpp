a4a1655aa865eef07d224513075c9b891a5c6f2e
Adnan Anjum <anjumcode12@gmail.com>
2026-08-13 12:40:59 +0500

    add quetions and answers for quiz game

--- quizGame.cpp
+++ quizGame.cpp
@@ -4,4 +4,27 @@ using std::cin;
 using std::cout;
 using std::string;
 
-int main() { return 0; }
+int main() {
+
+  string questions[] = {
+      "Which one is a low level language? ", "Which one is machine language?",
+      " What is the language of web? ", " Who is the creater of linux?"};
+  string options[4][4] = {{"A. Javascript", "B.Python", "C. C++", "D. PHP"},
+                          {"A. C", "B. C++", "C. Assembly", "D. Binary"},
+                          {"A. C#", "B. Javascript", "C. Python", "D. Rust"},
+                          {"A. Bill Gates", "B. Mark Zukerberg", "C. Elo Musk ",
+                           "D. Linus Torvalds"}};
+
+  char answerKey[] = {'C', 'D', 'B', 'D'};
+  int size = sizeof(questions) / sizeof(questions[0]);
+  char guess;
+  int score;
+
+  for (int i = 0; i < size; i++) {
+    cout << questions[i] << '\n';
+    for (int j = 0; j < sizeof(options[i]) / sizeof(options[i][0]); j++) {
+      cout << options[i][j] << '\n';
+    }
+  };
+  return 0;
+}
