3e04e7d6777ab18bcf57981cdbe8b3100622b8e3
Adnan Anjum <anjumcode12@gmail.com>
2026-08-14 01:41:06 +0500

    add function templates

---
+++ function_templates.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using std::cout;
+template <typename T, typename U>
+auto max(T a, U b) { return (a > b) ? a : b; }
+
+int main() {
+     cout << max(3, 2.345322) << '\n'; 
+    
+    }
+
