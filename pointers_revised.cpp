1bddcd5cf8e4c675e092c3701d78c7556eb398b6
Adnan Anjum <anjumcode12@gmail.com>
2026-08-14 16:38:40 +0500

    add one exercise of pointer to pointer

--- pointers_revised.cpp
+++ pointers_revised.cpp
@@ -17,6 +17,7 @@ int main() {
 
   int marks = 100;
 
+  cout << "Dereferenced marks are =======>" << *(&marks) << '\n';
   // first pointer
 
   int *point = &marks; // point has the adress  of value variable;
@@ -34,7 +35,30 @@ int main() {
   cout << "The address of pointer1 is " << &point << '\n';
   cout << "The address of pointer1 in pointer2 is " << point2 << '\n';
   cout << "The value pointer1 holds is :" << *point << '\n';
-  cout << "The value pointer2 holds is :" << **point2 << '\n';
+  cout << "The value pointer2 holds is :" << *(point2) << '\n';
+
+  cout << '\n';
+  cout << '\n';
+  cout << '\n';
+  // predict the output
+
+  int a = 5;
+  int *p = &a;
+  int **q = &p;
+
+  cout << '\n';
+  cout << '\n';
+  cout << '\n';
+  cout << "The address of a = " << &a << '\n'; // address of a ------------
+  cout << *p << '\n';  // value stored in the address stored in p which is
+                       // address of a  and value of a = 5  -------------
+  cout << **q << '\n'; // the value stored in the stored in the adress of q
+                       // which is the address of p , which stores the address
+                       // of a , value = 5 -----
+  cout << p << '\n';   // address stored in p , which is the address of a;
+  cout << *q << '\n';  // the value stored in the q which is the address of p
+  cout << &p << '\n'; // the address of p as its own in the memory as the adress
+                      // of q as its own
 
   return 0;
 }
