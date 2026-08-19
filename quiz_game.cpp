d8c54a9af7c5770efbfe8b3f09ae17841da11ee0
Adnan Anjum <anjumhere.dev@gmail.com>
2026-08-20 00:57:57 +0500

    removed c-styled arrays and used std::array

--- quiz_game.cpp
+++ quiz_game.cpp
@@ -1,3 +1,4 @@
+#include <array>
 #include <cctype>
 #include <iostream>
 
@@ -7,45 +8,44 @@ using std::string;
 
 int main() {
 
-  string questions[] = {
-      "Which one is a low level language? ", "Which one is machine language?",
-      " What is the language of web? ", " Who is the creator of linux?"};
+    std::array<string, 4> questions = {"Which one is a low level language? ", "Which one is machine language?",
+                                       " What is the language of web? ", " Who is the creator of linux?"};
 
-  string options[4][4] = {{"A. Javascript", "B.Python", "C. C++", "D. PHP"},
-                          {"A. C", "B. C++", "C. Assembly", "D. Binary"},
-                          {"A. C#", "B. Javascript", "C. Python", "D. Rust"},
-                          {"A. Bill Gates", "B. Mark Zukerberg",
-                           "C. Elon Musk ", "D. Linus Torvalds"}};
+    std::array<std::array<string, 4>, 4> options = {
+        {{"A. Javascript", "B.Python", "C. C++", "D. PHP"},
+         {"A. C", "B. C++", "C. Assembly", "D. Binary"},
+         {"A. C#", "B. Javascript", "C. Python", "D. Rust"},
+         {"A. Bill Gates", "B. Mark Zukerberg", "C. Elon Musk ", "D. Linus Torvalds"}}};
 
-  char answerKey[] = {'C', 'D', 'B', 'D'};
-  int size = sizeof(questions) / sizeof(questions[0]);
-  char guess;
-  int score = 0;
+    std::array<char, 4> answerKey = {'C', 'D', 'B', 'D'};
+    int size = questions.size();
+    char guess;
+    int score = 0;
 
-  for (int i = 0; i < size; i++) {
-    cout << questions[i] << '\n';
-    cout << "*********************************" << '\n';
+    for (int i = 0; i < size; i++) {
+        cout << questions[i] << '\n';
+        cout << "*********************************" << '\n';
 
-    for (int j = 0; j < sizeof(options[i]) / sizeof(options[i][0]); j++) {
-      cout << options[i][j] << '\n';
-    }
+        for (size_t j = 0; j < options[i].size(); j++) {
+            cout << options[i][j] << '\n';
+        }
 
-    cout << "Choose your answer :";
-    cin >> guess;
-    guess = toupper(guess);
+        cout << "Choose your answer :";
+        cin >> guess;
+        guess = static_cast<char>(toupper(guess));
 
-    if (guess == answerKey[i]) {
-      cout << '\n';
-      cout << "Your answer is correct." << '\n';
-      cout << '\n';
-      score++;
-    } else {
-      cout << "Wrong choice:::" << '\n';
-      cout << '\n';
-    }
+        if (guess == answerKey[i]) {
+            cout << '\n';
+            cout << "Your answer is correct." << '\n';
+            cout << '\n';
+            score++;
+        } else {
+            cout << "Wrong choice:::" << '\n';
+            cout << '\n';
+        }
 
-    cout << "Your total Score is :" << score << '\n';
-  };
+        cout << "Your total Score is :" << score << '\n';
+    };
 
-  return 0;
+    return 0;
 }
