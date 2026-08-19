f32d6816a69c467880f418e31cd1c469a5b300a8
Adnan Anjum <anjumhere.dev@gmail.com>
2026-08-20 03:37:51 +0500

    Add std::move instead of copying

--- oop_access_modifiers.cpp
+++ oop_access_modifiers.cpp
@@ -20,44 +20,45 @@ using std::string;
  */
 
 class Teacher {
-  // A private member restricts us from using the data outside the
-  // class; protected is used for inheritance.
-private:
-  double salary;
+    // A private member restricts us from using the data outside the
+    // class; protected is used for inheritance.
+  private:
+    double salary;
 
-public:
-  // Now that this section is public, we can access these members
-  // outside the class too.
-  string name;
-  string dept;
-  string subject;
+  public:
+    // Now that this section is public, we can access these members
+    // outside the class too.
+    string name;
+    string dept;
+    string subject;
 
-  void changeDept(string newDept) { dept = newDept; }
+    void changeDept(string newDept) { dept = std::move(newDept); }
 
-  // setter -> used to set the value of a private data member
-  void setSalary(double s) { salary = s; }
+    // setter -> used to set the value of a private data member
+    void setSalary(double s) { salary = s; }
 
-  // getter -> used to get the value of a private data member
-  double getSalary() { return salary; }
+    // getter -> used to get the value of a private data member
+    double getSalary() { return salary; }
 };
 
 int main() {
 
-  /*
-   * We can access and assign new values to the data members using
-   * the dot (.) operator.
-   */
-  Teacher t1;
-  t1.name = "Anjum";
-  t1.dept = "Computer Science";
-  t1.subject = "C++";
-  t1.changeDept("Psycology");
-  t1.setSalary(50000.50);
+    /*
+     * We can access and assign new values to the data members using
+     * the dot (.) operator.
+     */
+    Teacher t1;
+    t1.name = "Anjum";
+    t1.dept = "Computer Science";
+    t1.subject = "C++";
+    t1.subject = "Javscript";
+    t1.changeDept("Psycology");
+    t1.setSalary(50000.50);
 
-  cout << t1.name << '\n';
-  cout << t1.dept << '\n';
-  cout << t1.subject << '\n';
-  cout << t1.getSalary() << '\n';
+    cout << t1.name << '\n';
+    cout << t1.dept << '\n';
+    cout << t1.subject << '\n';
+    cout << t1.getSalary() << '\n';
 
-  return 0;
+    return 0;
 }
