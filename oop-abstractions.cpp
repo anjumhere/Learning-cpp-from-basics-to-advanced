2db5d0bad87c815a14dd4e00cc155e5005d4e9ad
Adnan Anjum <anjumhere.dev@gmail.com>
2026-08-21 21:20:58 +0500

    add  oop abstractions

--- oop-abstractions.cpp
+++ oop-abstractions.cpp
@@ -1,5 +1,9 @@
 /*
  * ----- Abstraction -----
+ *  Hiding all unnecessary details and only show the imporant stuff
+ *  ----- Ways to implement Abstraction
+ *  1. Access Modifiers are only of the ways to implement abstraction.
+ *
  */
 #include <iostream>
 #include <string>
