b2d5c6ca8ed29a974c617ef850fb564ea44f7404
Adnan Anjum <anjumhere.dev@gmail.com>
2026-08-25 23:40:46 +0500

    add stack container

---
+++ 09_stl/07_stack.cpp
@@ -0,0 +1,24 @@
+/*
+==================== Stack(LIFO) ===================================================
+Stack is a non-sequencial container which is last in first out structure.
+imagine books placed on top of each other,
+
+ stack ----->
+                   |         |<-------- element 4
+                   |_________|
+                   |         |<-------- element 3
+                   |_________|
+                   |         |<-------- element 2
+                   |_________|
+                   |         |<-------- element 1
+                   |_________|
+
+They can only be added from top and can only be removed from top
+    */
+#include <iostream>
+#include <stack>
+
+using std::cout;
+using std::stack;
+
+int main() { return 0; }
