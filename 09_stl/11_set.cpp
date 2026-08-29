dd2858a5c5cdb3fa314f98bc3a5d5a8bcb579ceb
Adnan Anjum <anjumhere.dev@gmail.com>
2026-08-29 23:43:33 +0500

    add lower-bound function

--- 09_stl/11_set.cpp
+++ 09_stl/11_set.cpp
@@ -30,9 +30,24 @@ int main() {
     s.insert(4);
     s.insert(5);
 
+    // if we try to insert same values again, they will be ignored , and size will not be effected.
+    // It will automatically give us the result in sorted order.
+    s.insert(1);
+    s.insert(2);
+
+    cout << s.size() << '\n';
     for (auto &val : s) {
         cout << val << " ";
     }
+
+    /* lower_bound() function => lower-bound function returns the minimum value with respect to the
+    provided value, if its not available , it will return the value just bigger than the required/asked
+    value
+    example => if we ask write lower-bound(4)=> it will return 4 and if 4 doesn't exist it will return a
+     number just bigger than 4 which is 5;
+    */
+
+    cout << "lower bound => " << *(s.lower_bound(5));
 
     return 0;
 }
